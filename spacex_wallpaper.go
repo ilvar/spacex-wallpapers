d3cc959f20e09c5fb0af6bf6923e254115f10b8b
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2018-06-22 23:06:02 +0100

    Initial code

---
+++ spacex_wallpaper.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+	"time"
+	"os/exec"
+	"math/rand"
+	"io/ioutil"
+	"net/http"
+	"encoding/xml"
+)
+
+func main() {
+	url := "https://www.flickr.com/services/feeds/photos_public.gne?id=130608600@N05&lang=en-us&format=atom"
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	type Feed struct {
+		XMLName xml.Name `xml:"feed"`
+		Entries []string `xml:"entry>content"`
+	}
+
+	feed := Feed{}
+
+	err = xml.Unmarshal([]byte(body), &feed)
+	if err != nil {
+		log.Fatal(err)
+	}
+	rand.Seed(time.Now().Unix())
+	randomized := feed.Entries[rand.Int() % len(feed.Entries)]
+
+	re := regexp.MustCompile("img src=\"https:\\/\\/farm\\d+\\.staticflickr\\.com\\/\\d+\\/(\\d+)_\\w+_m\\.jpg\"")
+	img_url := re.FindStringSubmatch(randomized)
+	if img_url == nil {
+		log.Fatal("Error parsing content: ", randomized)
+	}
+
+	full_url := "https://www.flickr.com/photos/spacex/" + img_url[1] + "/sizes/o/"
+
+	resp, err = http.Get(full_url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	re2 := regexp.MustCompile("<div id=\"allsizes\\-photo\">\\s+<img src=\"(https:\\/\\/.+staticflickr.com/(\\d+/)*(.+_o.jpg))\">\\s+</div>")
+	orig_url := re2.FindSubmatch(body)
+
+	file_url := string(orig_url[1])
+	file_path := "spacex_" + string(orig_url[len(orig_url) - 1])
+
+	fmt.Printf("%s\n", file_url)
+	fmt.Printf("%s\n", file_path)
+
+	err = exec.Command("wget", "-O", "/tmp/" + file_path, file_url).Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+
+	err = exec.Command("gsettings", "set", "org.gnome.desktop.background", "picture-uri", "file:///tmp/" + file_path).Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
